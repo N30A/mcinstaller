80f4cc1f73b81d51e79ae5e9548e5953070e16b0
Neo <95051018+N30A@users.noreply.github.com>
2024-04-28 13:59:14 +0200

    base implementation of the server interface for vanilla

--- servers/vanilla/vanilla.go
+++ servers/vanilla/vanilla.go
@@ -1,1 +1,15 @@
 package servers
+
+const (
+	versionManifestURL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+)
+
+type Vanilla struct{}
+
+func (v *Vanilla) Versions() ([]string, error) {
+	return nil, nil
+}
+
+func (v *Vanilla) DownloadURL(version string) (string, error) {
+	return "", nil
+}
