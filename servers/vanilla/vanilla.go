c3fd797201906d13047955ddbc841a8fb1b87ce2
Neo <95051018+N30A@users.noreply.github.com>
2024-04-28 15:44:05 +0200

    Implemented most vanilla server functions, Versions(), fetchVersionManifest(), fetchVersionPackage(), filterVersions()

--- servers/vanilla/vanilla.go
+++ servers/vanilla/vanilla.go
@@ -1,15 +1,61 @@
 package servers
 
+import (
+	"github.com/n30a/mcinstaller/helpers"
+)
+
 const (
-	versionManifestURL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+	versionManifestURL     = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+	firstVersionWithServer = "1.2.5"
 )
 
 type Vanilla struct{}
 
 func (v *Vanilla) Versions() ([]string, error) {
-	return nil, nil
+	versionManifest, err := fetchVersionManifest()
+	if err != nil {
+		return nil, err
+	}
+
+	versions := filterVersions(versionManifest)
+
+	return versions, nil
 }
 
 func (v *Vanilla) DownloadURL(version string) (string, error) {
 	return "", nil
+}
+
+func fetchVersionManifest() (*versionManifest, error) {
+	var versionManifest versionManifest
+
+	err := helpers.FetchAndDecodeJSON(versionManifestURL, &versionManifest)
+	if err != nil {
+		return nil, err
+	}
+
+	return &versionManifest, nil
+}
+
+func fetchVersionPackage(url string) (*versionPackage, error) {
+	var versionPackage versionPackage
+
+	err := helpers.FetchAndDecodeJSON(url, &versionPackage)
+	if err != nil {
+		return nil, err
+	}
+
+	return &versionPackage, nil
+}
+
+func filterVersions(versionManifest *versionManifest) []string {
+	indexOfFirstVersionWithServer := versionManifest.indexOf(firstVersionWithServer) + 1
+
+	versions := make([]string, indexOfFirstVersionWithServer)
+
+	for i := 0; i < len(versions); i++ {
+		versions[i] = versionManifest.Versions[i].ID
+	}
+
+	return versions
 }
