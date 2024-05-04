a473e9ccc8724fed17a59a804299712920567314
N30A <95051018+N30A@users.noreply.github.com>
2024-05-04 17:10:20 +0200

    Added cmd commands for listing servers and versions

--- cmd/list_server_versions.go
+++ cmd/list_server_versions.go
@@ -1,1 +1,38 @@
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/n30a/mcinstaller/servers"
+
+	"github.com/spf13/cobra"
+)
+
+var listServerVersionsCmd = &cobra.Command{
+	Use:                   "versions <server>",
+	Short:                 "List versions for the specified server",
+	Args:                  cobra.ExactArgs(1),
+	RunE:                  listServerVersions,
+	DisableFlagsInUseLine: true,
+}
+
+func listServerVersions(cmd *cobra.Command, args []string) error {
+	server := args[0]
+
+	serverType, err := servers.NewServer(server)
+	if err != nil {
+		return err
+	}
+
+	versions, err := serverType.Versions()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Listing all versions for %s...\n", server)
+	for _, version := range versions {
+		fmt.Print(version + " ")
+	}
+
+	return nil
+}
--- servers/server.go
+++ servers/server.go
@@ -1,5 +1,14 @@
 package servers
 
+import (
+	"fmt"
+
+	fabric "github.com/n30a/mcinstaller/servers/fabric"
+	forge "github.com/n30a/mcinstaller/servers/forge"
+	paper "github.com/n30a/mcinstaller/servers/paper"
+	vanilla "github.com/n30a/mcinstaller/servers/vanilla"
+)
+
 var SupportedServers = []string{
 	"vanilla",
 	"paper",
@@ -10,4 +19,28 @@ var SupportedServers = []string{
 type Server interface {
 	Versions() ([]string, error)
 	DownloadURL(version string) (string, error)
+}
+
+func NewServer(server string) (Server, error) {
+	var serverType Server
+
+	switch server {
+
+	case SupportedServers[0]:
+		serverType = &vanilla.Vanilla{}
+
+	case SupportedServers[1]:
+		serverType = &paper.Paper{}
+
+	case SupportedServers[2]:
+		serverType = &fabric.Fabric{}
+
+	case SupportedServers[3]:
+		serverType = &forge.Forge{}
+
+	default:
+		return nil, fmt.Errorf("unsupported server '%s'", server)
+	}
+
+	return serverType, nil
 }
