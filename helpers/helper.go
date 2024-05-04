469658bd2b289e03ca9a1fa79b7547c775cb6fdb
N30A <95051018+N30A@users.noreply.github.com>
2024-05-04 16:53:20 +0200

    Added function for downloading from url to disk

--- helpers/helper.go
+++ helpers/helper.go
@@ -3,7 +3,9 @@ package helpers
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func GetRequest(url string) (*http.Response, error) {
@@ -28,6 +30,27 @@ func FetchAndDecodeJSON(url string, data interface{}) error {
 	defer response.Body.Close()
 
 	err = json.NewDecoder(response.Body).Decode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DownloadFile(url, filePath string) error {
+	response, err := GetRequest(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
 	if err != nil {
 		return err
 	}
